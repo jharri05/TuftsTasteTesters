452534480785673b80e3510d335d7d540c50167f
Serena214365 <68926150+Serena214365@users.noreply.github.com>
2025-02-22 19:49:50 -0500

    Figuring it out

--- src/Dewick.js
+++ src/Dewick.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-// function About() {
-//   return (
-//     <div>
-//       <h1>About Tufts Taste Testers</h1>
-//       <p>This is where you can learn all about the mission of Tufts Taste Testers.</p>
-//     </div>
-//   );
-// }
+function Dewick() {
+  return (
+    <div>
+      <h1>About Tufts Taste Testers</h1>
+      <p>This is where you can learn all about the mission of Tufts Taste Testers.</p>
+    </div>
+  );
+}
 
-// export default About;
+export default Dewick;
--- src/welcome.js
+++ src/welcome.js
@@ -1,6 +1,9 @@
 import React from "react";
-import "./welcome.css";import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import { Routes } from "react-router-dom";
+import "./welcome.css";import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+// import { Routes } from "react-router-dom";
+// import Dewick from "./Dewick";
+
+
 function Welcome () { 
     return (
         <div >
@@ -8,10 +11,14 @@ function Welcome () {
                 <ul className = "nav">
                     <li>Home</li>
                     <li>Dining Menus</li>
-                    <li>Vote</li>
+                    {/* <li>Vote</li>
+                    <li><Link to="/">Home</Link></li> */}
+                    {/* <li><Link to="/Dewick">Dining Menus</Link></li>
+                    <li><Link to="/">About</Link></li> */}
+
                 </ul>
             </div>
-            <header comelassName="App-header">
+            <header className="App-header">
                 <h1>Welcome to Tufts Taste Testers!</h1>
                 <p> Where we serve the truth, one dining hall at a time</p>
                 <p>This is how busy it is out of 5 👤</p>
@@ -31,34 +38,34 @@ function Welcome () {
 //     );
 // }
 // function App() {
-    // return (
-    //   <Router>
-    //     <div className="App">
-    //       <header className="App-header">
-    //         {/* Navigation Bar */}
-    //         <nav>
-    //           <ul>
-    //             <li>
-    //               <Link to="/">Home</Link>
-    //             </li>
-    //             <li>
-    //               <Link to="/about">About</Link>
-    //             </li>
-    //             <li>
-    //               <Link to="/welcome">Welcome</Link>
-    //             </li>
-    //           </ul>
-    //         </nav>
+//     return (
+//       <Router>
+//         <div className="App">
+//           <header className="App-header">
+//             {/* Navigation Bar */}
+//             <nav>
+//               <ul>
+//                 <li>
+//                   <Link to="/">Home</Link>
+//                 </li>
+//                 <li>
+//                   <Link to="/Dewick">About</Link>
+//                 </li>
+//                 <li>
+//                   <Link to="/welcome">Welcome</Link>
+//                 </li>
+//               </ul>
+//             </nav>
   
-    //         {/* Routes */}
-    //         <Routes>
-    //           <Route path="/" exact element={Welcome} />
-    //           <Route path="/about" element={AboutPage} />
-    //           <Route path="/welcome" element={Welcome} />
-    //         </Routes>
-    //       </header>
-    //     </div>
-    //   </Router>
+//             {/* Routes */}
+//             <Routes>
+//               <Route path="/" exact element={<Welcome />} />
+//               <Route path="/Dewick" element={<Dewick />} />
+//               <Route path="/welcome" element={<Welcome />} />
+//             </Routes>
+//           </header>
+//         </div>
+//       </Router>
 //     );
 //   }
   
